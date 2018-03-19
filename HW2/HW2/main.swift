36beaab1e8f437bbbba444e2de1c66c366254f75
Juhun Kim <johnjhkim@hanmail.net>
2018-03-19 12:17:10 +0900

    The First Commit

---
+++ HW2/HW2/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  HW2
+//
+//  Created by Release on 2018. 3. 19..
+//  Copyright © 2018년 Release/jhkim. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+print("The first Commit")
+
+
